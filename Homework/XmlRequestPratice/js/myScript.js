521faf2a0f78eb04f6e58696b5c0e253bc24597d
Trung <trungsieunhan24@gmail.com>
2020-06-27 13:20:29 +0700

    done xml request

--- Homework/XmlRequestPratice/js/myScript.js
+++ Homework/XmlRequestPratice/js/myScript.js
@@ -1,30 +1,23 @@
 //https://www.c-sharpcorner.com/blogs/get-data-from-xml-content-using-javascript
 
 function readData(){
-  var parser, xmlDoc;
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        xmlDoc = this.responseText;
-      }
-    };
-    xhttp.open("GET", "file/cd_catalog.xml", true);
-    xhttp.send();
-  if(window.ActiveXObject)  
-  {  
-      doc = new ActiveXObject('Microsoft.XMLDOM'); // For IE6, IE5  
-      doc.async = 'false';  
-      doc.loadXML(xmlDoc);  
-  }  
-  else  
-  {  
-      var parser = new DOMParser();  
-      doc = parser.parseFromString(xmlDoc, 'text/xml'); // For Firefox, Chrome etc  
-  }  
-    
-  var x = doc.getElementsByTagName("CD");  
-  for (i = 0;i < x.length; i++)  
-  {  
+  var oXHR = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+
+  function reportStatus() {
+      if (oXHR.readyState == 4)               // REQUEST COMPLETED.
+          showTheList(this.responseXML);      // ALL SET. NOW SHOW XML DATA.
+  }
+
+  oXHR.onreadystatechange = reportStatus;
+  oXHR.open("GET", "file/cd_catalog.xml", true);      // true = ASYNCHRONOUS REQUEST (DESIRABLE), false = SYNCHRONOUS REQUEST.
+  oXHR.send();
+}      
+
+function showTheList(xml) {
+
+  var x = xml.getElementsByTagName('CD');       // THE XML TAG NAME.
+
+  for (var i = 0; i < x.length; i++) {
 
     var title, artist, country, company, price, year; 
               
@@ -34,21 +27,18 @@ function readData(){
     company =  x[i].getElementsByTagName("COMPANY")[0].childNodes[0].nodeValue   
     price = x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue  
     year = x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue    
-    $(document).ready(function () {
-      $('#employees').append('<tr>'
-      +'<td>'+title +'</td>'
-      +'<td class="">'+ artist+'</td>'
-      +'<td class="">'+country+'</td>'
-      +'<td class="">'+radioGender+'</td>'
-      +'<td class="">'
-      + company
-      +'</td>'
-      +'<td class="">'
-      + price
-      +'</td>'
-      +'<td class="">'+ year
-     +
-      '</td></tr>');
-});  
-  }  
-}      
+    $('#artists').append('<tr>'
+    +'<td>'+title +'</td>'
+    +'<td class="">'+ artist+'</td>'
+    +'<td class="">'+country+'</td>'
+    +'<td class="">'
+    + company
+    +'</td>'
+    +'<td class="">'
+    + price
+    +'</td>'
+    +'<td class="">'+ year
+   +
+    '</td></tr>');
+  }
+}
