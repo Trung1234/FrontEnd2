3fded6eb7c7f20a9b7753989cdaed1214367675a
unknown <Trung1234>
2020-07-08 08:06:24 -0700

    add angular folde

---
+++ T3H/Angular/my-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
